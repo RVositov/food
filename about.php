38d40d7e964eb9cfde4e0f4cd11bc52f0bd1bad9
Ravshan <ravshan488889@gmail.com>
2024-07-27 11:16:11 +0500

    Update about.php

--- about.php
+++ about.php
@@ -3,4 +3,8 @@ require_once("partials/header.php");
 require_once("partials/about-section.php");
 require_once("partials/teams.php");
 require_once("partials/footer.php");
+
+
+echo "changed";
+
 ?>
